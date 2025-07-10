e233d83f0d6cc42372d56a58e1708608f796db6f
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2025-07-10 12:39:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,13 +89,13 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://i.ibb.co/TqTjh7rm/Img2url-bot.jpg"
+    "START_IMG_URL", "https://i.ibb.co/d0tFFM5L/Img2url-bot.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://i.ibb.co/TqTjh7rm/Img2url-bot.jpg"
+    "PING_IMG_URL", "https://i.ibb.co/d0tFFM5L/Img2url-bot.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/14eb59ea7d31229d8d751.jpg"
-STATS_IMG_URL = "https://i.ibb.co/TqTjh7rm/Img2url-bot.jpg"
+STATS_IMG_URL = "https://i.ibb.co/d0tFFM5L/Img2url-bot.jpg"
 TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/923c1faac33d8c70335dc.jpg"
 TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6c66f8b192532fe758e82.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/ebc4dc6357be06e08a3ed.jpg"
